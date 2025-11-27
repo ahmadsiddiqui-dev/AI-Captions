af97067f1c3f3c3d50e1085725cae0af75b11b2b
RNF-User <you@example.com>
2025-11-27 16:28:40 +0530

    Fix nodemailer SMTP config + OTP email changes

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -16,8 +16,8 @@ const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 const sendOtpEmail = async (email, otp, name, type) => {
  const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 465,
-  secure: true, 
+  port: 587,
+  secure: false, 
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
